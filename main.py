f41e7eea358b37153a0608644f898ad17ae9b378
narinko33 <ttykzm@gmail.com>
2023-10-12 21:13:39 +0900

    1012

--- main.py
+++ main.py
@@ -1,8 +1,45 @@
 import tkinter as tk
+from random import randint as ri
 
 mouse_x, mouse_y = 0, 0
 
 
+# ランダム生成
+def block_make():
+    size = ("Arial", ri(25, 100))
+    make = cvs.create_text(
+        ri(50, 500),
+        ri(100, 400),
+        text="だんまくきたーーーーーーー",
+        fill="white",
+        tag="text",
+        font=size,
+    )
+    move()
+    cvs.after(ri(1500, 2000), block_make)
+
+
+# テキストブロック左から右に動く！
+def left():
+    cvs.move("text", 5, 0)
+    cvs.after(100, left)
+
+
+# テキストブロック上から下に動く！
+def up():
+    cvs.move("text", 0, 5)
+    cvs.after(100, up)
+
+
+def move():
+    num = ri(1, 2)
+    if num == 1:
+        left()
+    elif num == 2:
+        up()
+
+
+# マウスでの操作
 def mouse(e):
     global mouse_x, mouse_y
     cvs.delete("SCREEN")
@@ -15,12 +52,23 @@ root = tk.Tk()
 root.geometry("1280x720")
 frame = tk.Frame(root)
 frame.pack()
+font_size = ("Arial", ri(25, 100))
 
-cvs = tk.Canvas(frame, width=1000, height=600)
+# キャンパス描画
+cvs = tk.Canvas(frame, width=1300, height=800, bg="black")
 cvs.pack()
 
-img = tk.PhotoImage(file="avoid/star.png")
-cvs.create_image(100, 100, image=img, tag="SCREEN")
+# 自機描画
+img = tk.PhotoImage(file="avoid/star.png").subsample(2)
+cvs.create_image(50, 50, image=img, tag="SCREEN")
+
+# テキストブロック描画
+cvs.create_text(50, 100, text="わあ", fill="white", tag="text", font=font_size)
+move()
+# left()
+# up()
+
+block_make()
 
 
 root.bind("<Motion>", mouse)
